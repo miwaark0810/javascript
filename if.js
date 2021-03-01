44c4de1e347d460e66df516cc3f079a66501693f
miwaark0810 <a.niwa80@outlook.jp>
2021-03-01 18:29:18 +0900

    if命令

---
+++ if.js
@@ -0,0 +1,6 @@
+var x = 9;
+if(x >= 10){
+  console.log("変数xはは10以上です。");
+}else{
+  console.log("変数xはは10未満です。"); 
+}
---
+++ if_nest.js
@@ -0,0 +1,10 @@
+var x = 6;
+if(x >= 10){
+  if(x >= 20){
+    console.log("20以上です。");
+  }else{
+    console.log("10以上20未満です。");
+  }
+}else{
+  console.log("10未満です。");
+} 
