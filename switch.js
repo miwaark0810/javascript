6c14444db5154b8a25572ad94d0d6a8720b38022
miwaark0810 <a.niwa80@outlook.jp>
2021-03-01 18:37:45 +0900

    switch命令

---
+++ switch.js
@@ -0,0 +1,16 @@
+var rank = "D";
+switch(rank){
+  case"A":
+    console.log("Aランク");
+    break;
+  case"B":
+    console.log("Bランク");
+    break;
+  case"C":
+    console.log("Cランク");
+    break;
+  default:
+    console.log("ランク外");
+    break;
+
+}
