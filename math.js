63c12a1f2de661a5183c2001eb8de8e1e5f90d80
miwaark0810 <a.niwa80@outlook.jp>
2021-03-03 11:06:09 +0900

    Mathオブジェクト

---
+++ math.js
@@ -0,0 +1,7 @@
+console.log(Math.abs(-100));
+console.log(Math.min(20,40,69));
+console.log(Math.pow(5,3));
+console.log(Math.sign(5));
+console.log(Math.ceil(5.52));
+console.log(Math.floor(5.25));
+console.log(Math.round(5.13));
