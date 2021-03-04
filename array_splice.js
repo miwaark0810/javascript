e3fe3ea24f53f7c89af6dca88e7d435ef90ba686
miwaark0810 <a.niwa80@outlook.jp>
2021-03-04 09:19:11 +0900

    spliceメソッド

---
+++ array_splice.js
@@ -0,0 +1,7 @@
+var data = ["sato","takae","osada","hio","saitoh"]
+console.log(data.splice(3,2,"yaamda","suzuki"));
+console.log(data);
+console.log(data.splice(3,2));
+console.log(data);
+console.log(data.splice(1,0,"tanaka"));
+console.log(data);
