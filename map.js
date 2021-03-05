126d13b4efd9beb69276d3e8bd6eccfa160bc765
miwaark0810 <a.niwa80@outlook.jp>
2021-03-05 16:06:15 +0900

    map

--- map.js
+++ map.js
@@ -1,5 +1,26 @@
-var data = [2,3,4,5];
-var result = data.map(function(value,index,array){
-  return value * value;
-})
-console.log(result);
+let m = new Map();
+m.set("dog","ワンワン");
+m.set("cat","ニャー");
+m.set("mouse","チュー");
+
+console.log(m.size);
+console.log(m.get("dog"));
+console.log(m.has("cat"));
+
+for(let key of m.keys()){
+  console.log(key);
+}
+
+for(let value of m.values()){
+  console.log(value);
+}
+
+for(let [key,value] of m){
+  console.log(value);
+}
+
+m.delete("dog");
+console.log(m.size);
+
+m.clear();
+console.log(m.size);
