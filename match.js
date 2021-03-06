4a3cf6b6d23d2d2c750f6b0a7425a40d3fd31a99
miwaark0810 <a.niwa80@outlook.jp>
2021-03-06 18:24:08 +0900

    マルチラインモード

--- match.js
+++ match.js
@@ -1,4 +1,4 @@
-var p = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w-.\/?%&=]*)?/gi;
+var p = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w-.\/?%&=]*)?/g;
 var str = "サポートサイトはhttp://www.wings.msn.to/です。";
 str += "サンプル紹介サイトHTTP://www.web-deli.com/もよろしく！";
 var result = str.match(p);
