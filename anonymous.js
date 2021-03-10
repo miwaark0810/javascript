f20a22fdf782df6127157507dae20ba104c5733e
miwaark0810 <a.niwa80@outlook.jp>
2021-03-10 09:05:02 +0900

    匿名関数

---
+++ anonymous.js
@@ -0,0 +1,12 @@
+function arrayWalk(data,f){
+  for(var key in data){
+    f(data[key],key);
+  }
+}
+
+var ary = [1,2,4,8,16];
+arrayWalk(
+  ary,function(value,key){
+    console.log(key + "；" + value);
+  }
+)
